c4fd3c35d16081f534eb7614fd31945196ac83fe
Nimet Samur <nimetsamur.@gmail.com>
2022-05-28 10:36:33 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> people = new MyDictionary<string, int>();
+            people.Add("Nimet", 27);
+            Console.WriteLine(people.Length);
+            people.Add("Semih", 16);
+            people.Add("Mustafa", 31);
+
+            Console.WriteLine(people.Length);
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                Console.WriteLine("Key : " + people.Keys[i] + "  Value : " + people.Values[i]);
+                
+            }
+            
+        }
+    }
+}
